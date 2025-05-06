3f31c3826fd41f383022c903e63c18b1181cf237
rudivcoetzee <rudivoncoetzee@gmail.com>
2025-05-06 18:16:11 +0200

    changes to program.cs

--- BookManagerAPI/Program.cs
+++ BookManagerAPI/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Load configuration and register BookDbContext
+builder.Services.AddDbContext<BookDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Temporary test route to confirm everything works
+app.MapGet("/", () => "Book Manager API is running!");
 
 app.Run();
