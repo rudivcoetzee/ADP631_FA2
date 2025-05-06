b2f027d17292a6d2f25a3717bd6131bbcc176017
rudivcoetzee <rudivoncoetzee@gmail.com>
2025-05-06 20:29:12 +0200

    managed api endpoints

--- BookManagerAPI/Models/Books.cs
+++ BookManagerAPI/Models/Books.cs
@@ -1,6 +1,6 @@
 public class Book
 {
-    public int Id { get; set; }  // Primary Key
+    public int Id { get; set; }
     public string Title { get; set; }
     public string Author { get; set; }
     public string Genre { get; set; }
